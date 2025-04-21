dbe0e4a6525d498998919de0d54315d239cd6a19
Jason Seawall <jseawall@numurus.com>
2025-04-21 07:28:25 -0700

    3p2p0 RC5C updates

--- scripts/nepi_ai_yolov3_detection_node.py
+++ scripts/nepi_ai_yolov3_detection_node.py
@@ -20,18 +20,19 @@ import os
 import time
 import copy
 import sys
-import rospy
 import cv2
 import numpy as np
 
 import darknet
 
 from nepi_sdk import nepi_ros
-from nepi_sdk import nepi_msg
+from nepi_sdk import nepi_utils
 from nepi_sdk import nepi_img
 from nepi_sdk import nepi_ais
 
 from nepi_api.ai_if_detector import AiDetectorIF
+from nepi_api.messages_if import MsgIF
+
 
 # Define your PyTorch model and load the weights
 # model = ...
@@ -44,17 +45,21 @@ class Yolov3Detector():
     ### Node Initialization
     DEFAULT_NODE_NAME = "ai_yolov3" # Can be overwitten by luanch command
     def __init__(self):
-        #### APP NODE INIT SETUP ####
-        nepi_ros.init_node(name= self.DEFAULT_NODE_NAME)
-        self.node_name = nepi_ros.get_node_name()
+        ####  NODE Initialization ####
+        self.class_name = type(self).__name__
         self.base_namespace = nepi_ros.get_base_namespace()
-        self.node_namespace = self.base_namespace + self.node_name
-        nepi_msg.createMsgPublishers(self)
-        nepi_msg.publishMsgInfo(self,"Starting Initialization Processes")
-        ##############################
-        # Initialize parameters and fields.
+        self.node_name = nepi_ros.get_node_name()
+        self.node_namespace = nepi_ros.get_node_namespace()
+
+        ##############################  
+        # Create Msg Class
+        self.msg_if = MsgIF(log_name = self.class_name)
+        self.msg_if.pub_info("Starting Node Initialization Processes")
+
+        ##############################  
+        # Initialize Class Variables
         node_params = nepi_ros.get_param(self,"~")
-        nepi_msg.publishMsgInfo(self,"Starting node params: " + str(node_params))
+        self.msg_if.pub_info("Starting node params: " + str(node_params))
         self.all_namespace = nepi_ros.get_param(self,"~all_namespace","")
         if self.all_namespace == "":
             self.all_namespace = self.node_namespace
@@ -62,14 +67,14 @@ class Yolov3Detector():
         self.config_file_path = nepi_ros.get_param(self,"~config_file_path","")
         if self.config_file_path == "" or self.weight_file_path == "":
             nepi_msg.publishMsgWarn(self,"Failed to get required node info from param server: ")
-            rospy.signal_shutdown("Failed to get valid model info from param")
+            nepi_ros.signal_shutdown("Failed to get valid model info from param")
         else:
             # The ai_models param is created by the launch files load network_param_file line
             model_info = nepi_ros.get_param(self,"~ai_model","")
 
             if model_info == "":
                 nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: ")
-                rospy.signal_shutdown("Failed to get valid model file paths")
+                nepi_ros.signal_shutdown("Failed to get valid model file paths")
             else:
                 try: 
                     model_framework = model_info['framework']['name']
@@ -80,19 +85,19 @@ class Yolov3Detector():
                     self.proc_img_height = model_info['image_size']['image_height']['value']
                 except Exception as e:
                     nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: " + str(e))
-                    rospy.signal_shutdown("Failed to get valid model file paths")
+                    nepi_ros.signal_shutdown("Failed to get valid model file paths")
 
                 if model_framework != 'yolov3':
                     nepi_msg.publishMsgWarn(self,"Model not a yolov3 model: " + model_framework)
-                    rospy.signal_shutdown("Model not a valid framework")
+                    nepi_ros.signal_shutdown("Model not a valid framework")
 
-                nepi_msg.publishMsgInfo(self,"Loading model: " + self.node_name)
+                self.msg_if.pub_info("Loading model: " + self.node_name)
                 self.model = darknet.load_network(self.config_file_path, self.weight_file_path)
 
-                #nepi_msg.publishMsgInfo(self,"Waiting " + str(800) + " seconds for model to load")
+                #self.msg_if.pub_info("Waiting " + str(800) + " seconds for model to load")
                 #nepi_ros.sleep(800)
 
-                nepi_msg.publishMsgInfo(self,"Starting ai_if with defualt_config_dict: " + str(self.defualt_config_dict))
+                self.msg_if.pub_info("Starting ai_if with defualt_config_dict: " + str(self.defualt_config_dict))
                 self.ai_if = AiDetectorIF(model_name = self.node_name,
                                     framework = model_framework,
                                     description = model_description,
@@ -107,7 +112,7 @@ class Yolov3Detector():
 
                 #########################################################
                 ## Initiation Complete
-                nepi_msg.publishMsgInfo(self,"Initialization Complete")
+                self.msg_if.pub_info("Initialization Complete")
                 # Spin forever (until object is detected)
                 nepi_ros.spin()
                 #########################################################        
@@ -128,7 +133,7 @@ class Yolov3Detector():
         if nepi_img.is_gray(cv2_img):
             cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2BGR)
 
-        #nepi_msg.publishMsgInfo(self,":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
+        #self.msg_if.pub_info(":yolov5: Preprocessed image with image size: " + str(cv2_img.shape))
         # Create image dict with new image
         img_dict = dict()
         img_dict['cv2_img'] = cv2_img
@@ -191,7 +196,7 @@ class Yolov3Detector():
                         detect_dict['xmax'] = int(detect_dict['xmax'] * rescale_ratio)
                         detect_dict['ymax'] = int(detect_dict['ymax'] * rescale_ratio)
                         detect_dict_list.append(detect_dict)
-                        #nepi_msg.publishMsgInfo(self,"Got detect dict entry: " + str(detect_dict))
+                        #self.msg_if.pub_info("Got detect dict entry: " + str(detect_dict))
 
         return detect_dict_list
 
